de823dfd50842e5ecea44a8c21b31a119dcf8559
Felipe <fsaravia.avila@gmail.com>
2024-06-29 15:28:26 -0300

    Practica 2: prender y apagar leds de forma no bloqueante

---
+++ TP Final/Src/hd44780.c
@@ -0,0 +1,45 @@
+void HD44780_Init(uint8_t rows)
+{
+  dpRows = rows;
+  dpBacklight = LCD_BACKLIGHT;
+
+  dpFunction = LCD_4BITMODE | LCD_2LINE | LCD_5x8DOTS;
+
+
+  /* Wait for initialization */
+  DelayInit();
+  HAL_Delay(50);
+
+  ExpanderWrite(dpBacklight);
+  HAL_Delay(1000);
+
+  /* 4bit Mode */
+  Write4Bits(0x03 << 4);
+  DelayUS(4500);
+
+  Write4Bits(0x03 << 4);
+  DelayUS(4500);
+
+  Write4Bits(0x03 << 4);
+  DelayUS(4500);
+
+  Write4Bits(0x02 << 4);
+  DelayUS(100);
+
+  /* Display Control */
+  SendCommand(LCD_FUNCTIONSET | dpFunction);
+
+  dpControl = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
+  HD44780_Display();
+  HD44780_Clear();
+
+  /* Display Mode */
+  dpMode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
+  SendCommand(LCD_ENTRYMODESET | dpMode);
+  DelayUS(4500);
+
+  HD44780_CreateSpecialChar(0, special1);
+  HD44780_CreateSpecialChar(1, special2);
+
+  HD44780_Home();
+}
