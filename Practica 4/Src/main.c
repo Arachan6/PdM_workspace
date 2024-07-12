c1c0f028b412d2cb521621fc7727d1630ebf545a
Felipe <fsaravia.avila@gmail.com>
2024-07-11 21:57:29 -0300

    Update modularizacion sin terminar

--- Practica 4/Src/main.c
+++ Practica 4/Src/main.c
@@ -32,12 +32,7 @@
  */
 
 /* Private typedef -----------------------------------------------------------*/
-typedef enum{
-	BUTTON_UP,
-	BUTTON_FALLING,
-	BUTTON_DOWN,
-	BUTTON_RAISING,
-} debounceState_t;
+
 
 
 /* Private define ------------------------------------------------------------*/
@@ -47,16 +42,12 @@ typedef enum{
 
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-debounceState_t buttonState;
+
 delay_t d1;
 
 /* Private function prototypes -----------------------------------------------*/
 
-void debounceFSM_init();		// debe cargar el estado inicial
-void debounceFSM_update();	    // debe leer las entradas, resolver la lógica de
-					            // transición de estados y actualizar las salidas
-void buttonPressed();			// debe encender el LED
-void buttonReleased();		    // debe apagar el LED
+
 
 static void SystemClock_Config(void);
 
@@ -103,55 +94,7 @@ int main(void)
 	}
 }
 
-void debounceFSM_init(){
-	buttonState = BUTTON_UP;
-}
-void debounceFSM_update(){
-	switch(buttonState){
-		case BUTTON_UP:
-			if (BSP_PB_GetState(BUTTON_USER)){
-				buttonState = BUTTON_FALLING;
-				delayRead(&d1);
-			}
-			break;
-		case BUTTON_DOWN:
-			if (!BSP_PB_GetState(BUTTON_USER)){
-				buttonState = BUTTON_RAISING;
-				delayRead(&d1);
-			}
-			break;
-		case BUTTON_RAISING:
-			if (delayRead(&d1)){
-				if (!BSP_PB_GetState(BUTTON_USER)){
-					buttonState = BUTTON_UP;
-					buttonReleased();
-				} else {
-					buttonState = BUTTON_DOWN;
-				}
-			}
-			break;
-		case BUTTON_FALLING:
-			if (delayRead(&d1)){
-				if (BSP_PB_GetState(BUTTON_USER)){
-					buttonState = BUTTON_DOWN;
-					buttonPressed();
-				} else {
-					buttonState = BUTTON_UP;
-				}
-			}
-			break;
-		default:
-			Error_Handler();
-			break;
-	}
-}
 
-void buttonPressed(){
-	BSP_LED_On(LED1);
-}
-void buttonReleased(){
-	BSP_LED_Off(LED1);
-}
 
 /**
  * @brief  System Clock Configuration
