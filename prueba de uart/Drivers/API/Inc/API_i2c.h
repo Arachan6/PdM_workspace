f125e34358955c9db72d83a4bb9309db18ca39bc
Felipe <fsaravia.avila@gmail.com>
2024-07-28 17:41:44 -0300

    TP final: avance con modulo de GPS y UART

---
+++ prueba de uart/Drivers/API/Inc/API_i2c.h
@@ -0,0 +1,17 @@
+/*
+ * API_i2c.h
+ *
+ *  Created on: Jul 28, 2024
+ *      Author: felipe
+ */
+
+#ifndef API_INC_API_I2C_H_
+#define API_INC_API_I2C_H_
+
+#include "API_uart.h"
+
+bool_t I2C1_Init();
+static void I2C1_GPIO_Init();
+
+
+#endif /* API_INC_API_I2C_H_ */
